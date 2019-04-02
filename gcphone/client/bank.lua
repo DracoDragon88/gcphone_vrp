c66de4fa7ccb765be6cec126b57df49382962f71
Draco Dragon <8766125+DracoDragon88@users.noreply.github.com>
2019-04-02 19:04:25 +0200

    Update bank.lua
    replace bank update trigger for the simple banking resource.

--- gcphone/client/bank.lua
+++ gcphone/client/bank.lua
@@ -2,9 +2,9 @@
 --  Function APP BANK
 --====================================================================================
 
--- FOR vRP
-RegisterNetEvent('gcphone:setAccountMoney')
-AddEventHandler('gcphone:setAccountMoney', function(account)
+-- FOR vRP for the banking resource
+RegisterNetEvent('banking:updateBalance')
+AddEventHandler('banking:updateBalance', function(account)
     if account ~= nil then
         SendNUIMessage({event = 'updateBankbalance', banking = account})
     end
